c4653893b57af14dbd527d6328972a1f065e0862
vsingh18567 <59497793+vsingh18567@users.noreply.github.com>
2020-11-29 22:00:33 +0800

    login/register set up

--- homePage/urls.py
+++ homePage/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from .views import Home, Register
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('homePage.urls'))
+    path('', Home.as_view(), name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='homePage/login.html') , name='login'),
+    path('register/', Register.as_view(), name='register')
 ]
---
+++ homePage/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import redirect, render
+from django.views import View
+from .forms import RegisterForm
+from django.contrib import messages
+# Create your views here.
+
+class Home(View):
+
+    def get(self, request):
+
+        return render(request, 'homePage/home.html')
+
+
+class Register(View):
+
+    def get(self, request):
+        form = RegisterForm()
+        return render (request, 'homePage/register.html', {'form': form})
+    
+    def post(self, request):
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {username}!')
+            return redirect('login')
